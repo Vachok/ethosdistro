403c8c916ce57e8e0c1b059f3d0cab949fc02e1b
Vachok <143500@gmail.com>
2018-08-29 17:31:44 +0300

    feat(Enchance StopMailFlow):

---
+++ src/main/java/ru/vachok/ethosdistro/email/MessagesFromServer.java
@@ -0,0 +1,105 @@
+package ru.vachok.email;
+
+
+import ru.vachok.ethosdistro.email.Cleaner;
+import ru.vachok.messenger.MessageCons;
+import ru.vachok.messenger.MessageToUser;
+import ru.vachok.mysqlandprops.props.DBRegProperties;
+import ru.vachok.mysqlandprops.props.FileProps;
+import ru.vachok.mysqlandprops.props.InitProperties;
+
+import javax.mail.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.INFO;
+
+
+public class MessagesFromServer implements Callable<Message[]> {
+
+    private static final String SOURCE_CLASS = MessagesFromServer.class.getSimpleName();
+
+    private static final String S_N_N_S = "%s%n%n%s";
+
+
+    private boolean cleanMBox;
+
+    public MessagesFromServer(boolean cleanMBox) {
+        this.cleanMBox = cleanMBox;
+        MessageToUser messageToUser = new MessageCons();
+        messageToUser.info(SOURCE_CLASS, "cleanMBox is", cleanMBox + ".");
+    }
+
+    public MessagesFromServer() {
+        Logger.getLogger(SOURCE_CLASS).log(INFO, this.getClass().getTypeName());
+    }
+
+    @Override
+    public Message[] call() {
+        Message[] messages = new Message[0];
+        try{
+            if(cleanMBox){
+                throw new UnsupportedOperationException("Not Ready Yet");
+            }
+            messages = getInbox().getMessages();
+        }
+        catch(MessagingException e){
+
+            Logger.getLogger(SOURCE_CLASS).log(Level.WARNING, String.format(S_N_N_S, e.getMessage(), Arrays.toString(e.getStackTrace())));
+        }
+        return messages;
+    }
+
+    protected Folder getInbox() {
+        Properties mailProps = getSessionProps();
+        Authenticator authenticator = new Authenticator() {
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(mailProps.getProperty("user"), mailProps.getProperty("password"));
+            }
+        };
+        Session chkSess = Session.getDefaultInstance(mailProps, authenticator);
+        Store store = null;
+        try{
+            store = chkSess.getStore();
+            store.connect(mailProps.getProperty("host"), mailProps.getProperty("user"), mailProps.getProperty("password"));
+        }
+        catch(MessagingException e){
+            Logger.getLogger(SOURCE_CLASS).log(Level.WARNING, String.format(S_N_N_S, e.getMessage(), Arrays.toString(e.getStackTrace())));
+        }
+        Folder inBox = null;
+        try{
+            inBox = Objects.requireNonNull(store).getFolder("Inbox");
+            inBox.open(Folder.READ_WRITE);
+            Logger.getLogger(Cleaner.class.getSimpleName()).log(INFO, inBox.getMessageCount() + " inbox size");
+            return inBox;
+        }
+        catch(MessagingException e){
+            Logger.getLogger(SOURCE_CLASS).log(Level.WARNING, String.format(S_N_N_S, e.getMessage(), Arrays.toString(e.getStackTrace())));
+        }
+        throw new UnsupportedOperationException("Inbox not available :(");
+    }
+
+    private Properties getSessionProps() {
+        InitProperties initProperties = new DBRegProperties("mail-regru");
+        Properties sessionProps = initProperties.getProps();
+        sessionProps.setProperty("NewSessionStarted", new Date().toString());
+        saveProps(sessionProps);
+        return sessionProps;
+    }
+
+
+    private void saveProps(Properties sessionProps) {
+        InitProperties initProperties = new FileProps(SOURCE_CLASS);
+        initProperties.setProps(sessionProps);
+        initProperties.getProps();
+        initProperties.setProps(sessionProps);
+    }
+
+}
--- src/main/java/ru/vachok/ethosdistro/parser/ParsingStart.java
+++ src/main/java/ru/vachok/ethosdistro/parser/ParsingStart.java
@@ -1,9 +1,7 @@
 package ru.vachok.ethosdistro.parser;
 
 
-import ru.vachok.ethosdistro.AppStarter;
 import ru.vachok.ethosdistro.ConstantsFor;
-import ru.vachok.ethosdistro.email.ECheck;
 import ru.vachok.ethosdistro.util.DBLogger;
 import ru.vachok.ethosdistro.util.FileLogger;
 import ru.vachok.ethosdistro.util.TForfs;
@@ -24,152 +22,149 @@ import java.util.logging.Logger;
  @since 23.08.2018 (16:48) */
 public class ParsingStart implements Runnable {
 
-   /**
-    {@link Parsers}
-    */
-   private Parsers parsers;
+    /**
+     <b>Проверка наличия временной остановки</b>
+     */
+//    static boolean shouldIWork = ECheck.isShouldIWork();
 
-   /**
-    Параметр {@code -t on}
-    */
-   private boolean test;
+    /**
+     Class Simple Name
+     */
+    private static final String SOURCE_CLASS = ParsingStart.class.getSimpleName();
 
-   /**
-    URL, как строка
-    */
-   private final String urlAsString;
+    /**
+     {@link Logger}
+     */
+    private static final Logger LOGGER = Logger.getLogger(SOURCE_CLASS);
 
-   /**
-    Class Simple Name
-    */
-   private static final String SOURCE_CLASS = ParsingStart.class.getSimpleName();
+    /**
+     {@link DBLogger}
+     */
+    private static final MessageToUser TO_USER_DATABASE = new DBLogger();
 
-   /**
-    {@link Logger}
-    */
-   private static final Logger LOGGER = Logger.getLogger(SOURCE_CLASS);
+    /**
+     URL, как строка
+     */
+    private final String urlAsString;
 
-   /**
-    {@link DBLogger}
-    */
-   private static final MessageToUser MESSAGE_TO_USER = new DBLogger();
+    /**
+     {@link Parsers}
+     */
+    private Parsers parsers;
 
-   /*Constru*/
+    /**
+     Параметр {@code -t on}
+     */
+    private final boolean test;
 
-   /**
-    <b>Конструктор</b>
+    /*Constru*/
 
-    @param urlAsString url как строка
-    */
-   public ParsingStart(String urlAsString) {
-      this.parsers = new ParseJsonAsUsualString();
-      this.urlAsString = urlAsString;
-      this.parsers = new ParseJsonAsUsualString();
-   }
+    /**
+     Конструктор
 
-   /**
-    Конструктор
+     @param parsers     чем парсим {@link Parsers}
+     @param urlAsString url как строка
+     */
+    public ParsingStart(Parsers parsers, String urlAsString) {
+        this.parsers = parsers;
+        this.urlAsString = urlAsString;
+        test = false;
+    }
 
-    @param urlAsString url как строка
-    @param fileName    файл, для записи (имя)
-    */
-   public ParsingStart(String urlAsString, String fileName) {
-      if(fileName==null){
-         fileName = "answer.json";
-      }
-      this.urlAsString = urlAsString;
-      this.parsers = new ParseToFile(fileName);
-   }
+    /**
+     Конструктор
 
-   /**
-    Конструктор
+     @param s    url как строка
+     @param test test - запуск с <i>обратным !</i> условием.
+     */
+    public ParsingStart(String s, boolean test) {
+        this.urlAsString = s;
+        this.test = test;
+    }
 
-    @param parsers     чем парсим {@link Parsers}
-    @param urlAsString url как строка
-    */
-   public ParsingStart(Parsers parsers, String urlAsString) {
-      this.parsers = parsers;
-      this.urlAsString = urlAsString;
-   }
+    /**
+     <b>Старт парсинга</b>
+     <p>
+     1. Создание инстанции {@link ParseToFile} <br>
+     2. Создание answer.json {@link Parsers#startParsing(URL)} <br>
+     3. Вывод сообщения через {@link #TO_USER_DATABASE} - {@link ConstantsFor#RCPT} <br>
+     4. {@link #sendRes(boolean)} <br>
 
-   /**
-    Конструктор
+     @see TForfs
+     */
+    @Override
+    public void run() {
+        this.parsers = new ParseToFile();
+        URL url = getUrlFromStr();
+        parsers.startParsing(url);
+        String s = new TForfs().toStringFromArray(ConstantsFor.RCPT);
+        TO_USER_DATABASE.info(SOURCE_CLASS, "email RCPTs", s);
+//        if(shouldIWork){
+            sendRes(this.test);
+//        }
+//        else{
+//            Thread.currentThread().interrupt();
+//        }
+    }
 
-    @param s    url как строка
-    @param test test - запуск с <i>обратным !</i> условием.
-    */
-   public ParsingStart(String s, boolean test) {
-      this.urlAsString = s;
-      this.test = test;
-      final ECheck eCheck = ECheck.getI();
-      eCheck.run();
-   }
+    private URL getUrlFromStr() {
+        URL url = null;
+        try{
+            url = new URL(urlAsString);
+        }
+        catch(MalformedURLException e){
+            LOGGER.throwing(SOURCE_CLASS, "getSite", e);
+        }
+        return url;
+    }
 
-   @Override
-   public void run() {
-      boolean work = ECheck.isShouldIWork();
-      MESSAGE_TO_USER.info(SOURCE_CLASS, work + " work", ECheck.offSender());
-      this.parsers = new ParseToFile();
-      URL url = getUrlFromStr();
-      parsers.startParsing(url);
-      String s = new TForfs().toStringFromArray(ConstantsFor.RCPT);
-      MESSAGE_TO_USER.info(SOURCE_CLASS, "email recep", s);
-      if(work){
-         sendRes(this.test);
-      }
-      else{
-         MESSAGE_TO_USER.errorAlert(SOURCE_CLASS, work + "work", ECheck.offSender());
-         Thread.currentThread().interrupt();
-      }
-   }
+    /**
+     <b>Отправить уведомление</b>
+     <p>
+     Если {@link #test} правда. Проверить {@link ParsingFinalize#call()} и инвертировать его выдачу. <i>(ложь-на-правда; правда-на-ложь)</i><br>
+     Если нет - продолжить с уловием {@link ParsingFinalize#call()}.
+     <p>
+     {@code MessageToUser emailS = new ESender(ConstantsFor.RCPT);} - опреденение способа отправки e-mail сообщения.<br>
+     Если условия удовлетворяют ({@link ParsingFinalize#call()} = <b>true</b>, записать через {@link MessageToUser} 2 лога: в базу (<i>server202.reg</i>) и файл (<i>log.log</i>) <br>
+     Если нет - отравить уведомнение списку {@link ConstantsFor#RCPT}
 
-   private URL getUrlFromStr() {
-      URL url = null;
-      try{
-         url = new URL(urlAsString);
-      }
-      catch(MalformedURLException e){
-         LOGGER.throwing(SOURCE_CLASS, "getSite", e);
-      }
-      return url;
-   }
+     @param callTest {@link #test}
+     @see ParsingFinalize
+     @see TForfs
+     */
+    private void sendRes(boolean callTest) {
+        Boolean call;
+        if(callTest){
+            call = !new ParsingFinalize().call();
+        }
+        else{
+            call = new ParsingFinalize().call();
+        }
+        File file = new File("answer.json");
+        MessageToUser emailS = new ESender(ConstantsFor.RCPT);
+        MessageToUser fileLogger = new FileLogger();
+        fileLogger.info(SOURCE_CLASS, "ConstantsFor.RCPT",
+                "Mailing list (" +
+                        ConstantsFor.RCPT.size() + "):\n" +
+                        new TForfs().toStringFromArray(ConstantsFor.RCPT));
+        if(call){
+            String statisticsProb = new Date(file.lastModified()) + "\n" + file.getAbsolutePath();
+            String freeSpaceOnDisk = file.getFreeSpace() / ConstantsFor.MEGABYTE + " free space in Megabytes";
 
-   private void sendRes(boolean callTest) {
-      Boolean call;
-      if(callTest){
-         call = !new ParsingFinalize().call();
-      }
-      else{
-         call = new ParsingFinalize().call();
-      }
-      File file = new File("answer.json");
-      MessageToUser emailS = new ESender(ConstantsFor.RCPT);
-      MessageToUser log = new FileLogger();
-      MESSAGE_TO_USER.info(SOURCE_CLASS, "INFO", "ConstantsFor.RCPT.size() = " + ConstantsFor.RCPT.size() + "\n" +
-            "Uptime = "
-            + TimeUnit.MILLISECONDS
-            .toHours(System.currentTimeMillis() -
-                  AppStarter.getStartLong()) +
-            " started at " +
-            new Date(AppStarter.getStartLong()));
-      log.info(SOURCE_CLASS, "ConstantsFor.RCPT.size() = ", ConstantsFor.RCPT.size() + "");
-      if(call){
-         MESSAGE_TO_USER.info(SOURCE_CLASS, file
-                     .getFreeSpace() / ConstantsFor.MEGABYTE +
-                     " free space",
-               new Date(file.lastModified()) + "\n" + file.getAbsolutePath());
-         log.info(SOURCE_CLASS, file
-                     .getFreeSpace() / ConstantsFor.MEGABYTE +
-                     " free space",
-               new Date(file.lastModified()) + "\n" + file.getAbsolutePath());
-         Thread.currentThread().interrupt();
-      }
-      else{
-         emailS.errorAlert("ALARM!", "Condition not mining", "NO MINING! " + urlAsString);
-         log.errorAlert("ALARM!", "Condition not mining", "NO MINING! " + urlAsString);
-      }
-      MESSAGE_TO_USER.infoNoTitles(ECheck.getI().toString());
-      Thread.currentThread().interrupt();
-   }
+            TO_USER_DATABASE.info(SOURCE_CLASS,
+                    "END (Uptime = " + ( float ) (System
+                                                          .currentTimeMillis() - ConstantsFor
+                            .START_TIME_IN_MILLIS) / TimeUnit.HOURS.toMillis(1) + " hrs)",
+                    freeSpaceOnDisk + "\n" + statisticsProb);
+            fileLogger.info(SOURCE_CLASS, freeSpaceOnDisk, statisticsProb);
+            Thread.currentThread().interrupt();
+        }
+        else{
+            emailS.errorAlert("ALARM!", "Condition not mining", "NO MINING! " + urlAsString);
+            fileLogger.errorAlert("ALARM!", "Condition not mining", "NO MINING! " + urlAsString);
+            Thread.currentThread().interrupt();
+        }
+
+    }
 }
 
